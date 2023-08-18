924578770171d92c5d5478b56f2d9ca006841027
shelly024 <136613866+shelly024@users.noreply.github.com>
2023-08-18 21:12:07 +0800

    Update DebugExercise1.java

--- lab2/DebugExercise/DebugExercise1.java
+++ lab2/DebugExercise/DebugExercise1.java
@@ -6,7 +6,7 @@ package DebugExercise;
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
-      if (bottom == 0) {
+        if (bottom == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed");
         }
         float quotient = (float) top / bottom;
@@ -21,9 +21,14 @@ public class DebugExercise1 {
         System.out.println("round(" + t + "/" + b + ")=" + result);
 
         int t2 = 9;
-        int b2 = 4;
+        int b2 = 100;
         int result2 = divideThenRound(t2, b2);
         System.out.println("round(" + t2 + "/" + b2 + ")=" + result2);
+
+        int t4 = 9;
+        int b4 = 0;
+        int result4 = divideThenRound(t4, b4);
+        System.out.println("round(" + t4 + "/" + b4 + ")=" + result4);
 
         int t3 = 3;
         int b3 = 4;
